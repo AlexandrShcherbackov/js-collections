cea217115699cbd1bb8413ca63c68e90b850d18f
Alexandr <jsprog2017@yandex.ru>
2021-01-11 13:03:24 +0300

    added functions isEmpty, has

--- __tests__/listHelper.test.js
+++ __tests__/listHelper.test.js
@@ -1,11 +1,21 @@
-import { makeList, getFirstNode, getRestOfList } from '../src/helpers/listHelper'
+import { makeList, getFirstNode, getRestOfList, isEmpty, has } from '../src/helpers/listHelper'
 
-describe('Basic lists test', () => {
-    test('are our functions work?', () => {
-        const list = makeList(1,2,3,4);
+const list = makeList(1,2,3,4);
+
+describe('Basic list\'s asbstractions test', () => {
+    it('Do list constructor and getters work?', () => {
         expect(getFirstNode(list)).toBe(1);
         expect(getRestOfList(list)).toEqual(makeList(2,3,4))
     });
 
+    it('Does function isEmpty work', () => {
+        expect(isEmpty(list)).toBeFalsy();
+        expect(isEmpty(makeList())).toBeTruthy();
+    });
 
+    it('Does function has work', () => {
+        expect(has(list, 1)).toBeTruthy();
+        expect(has(list, 3)).toBeTruthy();
+        expect(has(list, 10)).toBeFalsy();
+    });
 });
--- src/helpers/listHelper.js
+++ src/helpers/listHelper.js
@@ -2,6 +2,9 @@ import { makePair, getFirst, getSecond } from '../helpers/pairsHelper'
 
 const makeNode = (first, second) => makePair(first, second); 
 
+//The list must be a sequential connection of elements 
+//and have an end marker
+
 // makeList recieve pairs and return list 
 const makeList = (...data) => {
     const revresedData = data.reverse();
@@ -14,4 +17,22 @@ const getFirstNode = (list) => getFirst(list);
 
 const getRestOfList = (list) => getSecond(list);
 
-export { makeList, getFirstNode, getRestOfList };
+const isEmpty = (list) => list === null;
+
+// function has recive list and some element.
+// Function check is element in list
+
+const has = (list, element) => {
+    if (isEmpty(list)) {
+        return false;
+    }
+
+    const firstelement = getFirstNode(list);
+    const rest = getRestOfList(list);
+
+    return firstelement === element
+        ? true
+        : has(rest, element);
+};
+
+export { makeNode, makeList, getFirstNode, getRestOfList, isEmpty, has };
