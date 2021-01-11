a97d364543e826865e17874718cfbed72909afdf
Alexandr <jsprog2017@yandex.ru>
2021-01-11 13:50:15 +0300

    added revese

--- __tests__/listHelper.test.js
+++ __tests__/listHelper.test.js
@@ -1,6 +1,6 @@
-import { makeList, getFirstNode, getRestOfList, isEmpty, has } from '../src/helpers/listHelper'
+import { makeList, getFirstNode, getRestOfList, isEmpty, has, reverse } from '../src/helpers/listHelper'
 
-const list = makeList(1,2,3,4);
+const list = makeList(1, 2, 3, 4);
 
 describe('Basic list\'s asbstractions test', () => {
     it('Do list constructor and getters work?', () => {
@@ -17,5 +17,10 @@ describe('Basic list\'s asbstractions test', () => {
         expect(has(list, 1)).toBeTruthy();
         expect(has(list, 3)).toBeTruthy();
         expect(has(list, 10)).toBeFalsy();
+    });
+
+    it('Does reverse work', () => {
+        expect(reverse(list)).toEqual(makeList(4, 3, 2, 1));
+        expect(reverse(makeList())).toEqual(makeList());
     });
 });
--- src/helpers/listHelper.js
+++ src/helpers/listHelper.js
@@ -7,9 +7,9 @@ const makeNode = (first, second) => makePair(first, second);
 
 // makeList recieve pairs and return list 
 const makeList = (...data) => {
-    const revresedData = data.reverse();
+    const reversedData = data.reverse();
     const reducer = (acc, item) => makePair(item, acc);
-    return revresedData.reduce(reducer, null);
+    return reversedData.reduce(reducer, null);
 };
 
 // getFirstNode recieve list and return first node
@@ -35,4 +35,29 @@ const has = (list, element) => {
         : has(rest, element);
 };
 
-export { makeNode, makeList, getFirstNode, getRestOfList, isEmpty, has };
+//recieve list and return reversed list
+const reverse = (list) => {
+    const iter = (elements, acc) => {
+        if (isEmpty(elements)) {
+            return acc;
+        }
+
+        const newAcc = makePair(getFirst(elements), acc);
+        const rest = getRestOfList(elements);
+
+        return iter(rest, newAcc); 
+    };
+
+
+    return iter(list, makeList());
+};
+
+export { 
+    makeNode,
+    makeList,
+    getFirstNode,
+    getRestOfList,
+    isEmpty,
+    has,
+    reverse,
+};
